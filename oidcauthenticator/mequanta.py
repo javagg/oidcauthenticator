237c4b0813e42e8cd4d28a6147a6f660901bc96d
alex <lu.lee05@gmail.com>
2015-10-26 17:51:14 +0800

    add fixed_user options

--- oidcauthenticator/mequanta.py
+++ oidcauthenticator/mequanta.py
@@ -1,5 +1,8 @@
 from jupyterhub.auth import LocalAuthenticator
 from .oidc import OidcAuthenticator
+import os
+from traitlets import Unicode
+from tornado import gen
 
 class MeQuantaOidcAuthenticator(OidcAuthenticator):
     login_service = "MeQuanta"
@@ -7,6 +10,17 @@ class MeQuantaOidcAuthenticator(OidcAuthenticator):
     oidc_callback_url = 'https://localhost:8000/hub/oidc_callback'
     client_id_env = 'MEQUANTA_CLIENT_ID'
     client_secret_env = 'MEQUANTA_CLIENT_SECRET'
+    fixed_user_env = 'MEQUANTA_FIXED_USER'
+    fixed_user = Unicode(config=True)
+    def _fixed_user_default(self):
+        return os.getenv(self.fixed_user_env, None)
+
+    @gen.coroutine
+    def authenticate(self, handler):
+        username = super(MeQuantaOidcAuthenticator, self).authenticate(handler)
+        if username is not None and self.fixed_user is not None:
+            username = self.fixed_user
+        return username
 
 class LocalMeQuantaOidcAuthenticator(LocalAuthenticator, MeQuantaOidcAuthenticator):
     pass
